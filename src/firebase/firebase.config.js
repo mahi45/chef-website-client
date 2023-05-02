fbb0205e2708d42511fb95d9768dd9d33c4445d7
Mohidul Hasan <mahi110145@gmail.com>
2023-05-02 23:26:51 +0600

    React and firebase install also firebase config update

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCW1zfBGl29puwJ8OZmtqZwnuXROD6KCL0",
+  authDomain: "food-recipe-7b8b9.firebaseapp.com",
+  projectId: "food-recipe-7b8b9",
+  storageBucket: "food-recipe-7b8b9.appspot.com",
+  messagingSenderId: "708902162533",
+  appId: "1:708902162533:web:9b5f21b3310ac757e4768c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
