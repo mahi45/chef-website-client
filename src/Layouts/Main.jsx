d284cab848ef7d851871436a70ba8d42b21e05ee
Mohidul Hasan <mahi110145@gmail.com>
2023-05-03 02:16:48 +0600

    recipe category loading at left nav

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -4,6 +4,7 @@ import Footer from "../Pages/Shared/Footer/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import Banner from "../Pages/Shared/Banner/Banner";
 import "../App.css";
+import LeftNav from "../Pages/Shared/LeftNav/LeftNav";
 
 const Main = () => {
   return (
@@ -12,7 +13,10 @@ const Main = () => {
       <Banner></Banner>
       <Container>
         <Row>
-          <Col lg={12}>
+          <Col lg={3}>
+            <LeftNav></LeftNav>
+          </Col>
+          <Col lg={9}>
             <h2 className="pt-3 pb-2 border-bottom">Our Chefs</h2>
           </Col>
         </Row>
---
+++ src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/recipe_categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      <h2 className="pt-3 pb-2 border-bottom">Recipe Category</h2>
+      <div className="ps-4">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`recipe_category/${category.id}`}
+              className="text-decoration-none text-black"
+            >
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftNav;
