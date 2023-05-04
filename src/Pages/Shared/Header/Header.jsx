ee4a007ee546ccea1b5eeb28f2c292871649c58f
Mohidul Hasan <mahi110145@gmail.com>
2023-05-04 08:42:00 +0600

    Website name updated and shown at the website title, every page title has been updated, favicon has been updated

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const loc = useLocation();
+  const [title, setTittle] = useState("");
+
+  useEffect(() => {
+    if (loc.pathname === "/") {
+      document.title = `Star Chef`;
+      setTittle(document.title);
+    } else {
+      document.title = `${loc.pathname.replace("/", "")}`;
+      setTittle(document.title);
+    }
+    if (loc.state) {
+      document.title = loc.state;
+      setTittle(document.title);
+    }
+  }, [loc.pathname]);
+
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="/">My Recipe</Navbar.Brand>
+          <Navbar.Brand href="/">Star Chef</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto">
